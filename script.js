4a295486186971378ac90c26fe3f389ee230036b
Sohidul Islam Ananto <anonto1080@gmail.com>
2023-11-15 23:47:55 +0600

    add all

---
+++ script.js
@@ -0,0 +1,31 @@
+const btnGen = document.getElementById("ganBtn")
+
+const passwordBox = document.getElementById("Password");
+const lenGht = 12;
+
+const upperCase = "ASDFGHJKLQWERTYUIOPZXCVBNM";
+const lowerCase = "asdfghjklqwertyuiopzxcvbnm";
+
+const number = "0123456789";
+const symbol = "!@#$%^&*()_+{}[]:;-=";
+
+
+const allChars = upperCase + lowerCase + number + symbol;
+
+
+function createPassword(){
+    let password = "";
+    password += upperCase[ Math.floor(Math.random() * upperCase.length)]
+    password += lowerCase[ Math.floor(Math.random() * lowerCase.length)]
+    password += number[ Math.floor(Math.random() * number.length)]
+    password += symbol[ Math.floor(Math.random() * symbol.length)]
+
+    while(lenGht > password.length){
+        password += allChars[ Math.floor(Math.random() * allChars.length)]
+    }
+    passwordBox.value = password;
+}
+function copyPassword(){
+    passwordBox.select();
+    document.execCommand("copy")
+}
